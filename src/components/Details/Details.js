3e900dc422f28bdaa8499272c9306372bf0569ba
Timelas <lom.a.s@icloud.com>
2023-06-15 16:54:28 +0300

    test

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -6,7 +6,7 @@ function Details() {
   return (
     <section className="details" id="details">
       <img src={details} alt="детали" className="details__header"></ img>
-      <p className="details__text">По всем вопросам обращайтесь к организаторам<br/>Даниил +7 911 285-26-98<br/>и Лилия +7 999-232-36-27</p>
+      <p className="details__text">По всем вопросам обращайтесь к организаторам<br/>Даниил <a className="details__tel" href="+7(911)285-26-98">+7 911 285-26-98</a><br/>и Лилия <a className="details__tel" href="+7(999)232-36-27">+7 999 232-36-27</a></p>
     </section>
   );
 }
